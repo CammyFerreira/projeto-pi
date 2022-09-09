dae7b7896c6503e53154f17ca83cdb67c95038d2
kahturin <karinaalveslima@icloud.com>
2022-09-08 22:32:16 -0300

    CRUD de produtos

---
+++ resources/views/produto/index.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <title>Lista de produtos</title>
+</head>
+<body class="container mt-5 bg-dark">
+    <h1 style="color: white">Lista de Produtos</h1>
+    <a href=" {{ Route('produto.criar') }}" class="btn btn-lg btn-light">Criar Produto  </a>
+</body>
+</html>
