1db56b2557a1376d8bac70ce200fd9d08d4bbf58
CamilaAFerreira <camila.aferreira@tksolution.com.br>
2022-09-29 19:09:49 -0300

    Ajuste do banner

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,17 +4,12 @@
     <!-- Start Banner Hero -->
     <div class="carousel">
     <div id="template-mo-zay-hero-carousel" class="carousel slide" data-bs-ride="carousel">
-        <ol class="carousel-indicators">
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
-            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
-        </ol>
         <div class="carousel-inner">
             <div class="carousel-item active">
                 <div class="container">
                     <div class="row p-5">
                         <div class="mx-auto col-md-8 col-lg-6 order-lg-last">
-                            <img class="img-fluid" src="/img/banner2.jpg" alt="">
+                            <img class="img-fluid" src="/img/banner2.png" alt="">
                         </div>
                         <div class="col-lg-6 mb-0 d-flex align-items-center">
                             <div class="text-align-left align-self-center">
@@ -74,6 +69,11 @@
             <i class="fas fa-chevron-right"></i>
         </a>
     </div>
+    <ol class="carousel-indicators">
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="0" class="active"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="1"></li>
+            <li data-bs-target="#template-mo-zay-hero-carousel" data-bs-slide-to="2"></li>
+    </ol>
 </div>
     <!-- End Banner Hero -->
 
