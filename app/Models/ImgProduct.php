88f6b28d6c652bb7ec7754560c3fece244e5b265
REBECA RODRIGUES DE PRIMO <rebeca.rprimo@senacsp.edu.br>
2022-11-09 22:25:55 -0200

    model ImgProduct criada

---
+++ app/Models/ImgProduct.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ImgProduct extends Model
+{
+    use HasFactory;
+
+    protected $timestamp = false;
+    protected $fillable = ['IMAGEM_ID', 'IMAGEM_ORDEM', 'PRODUTO_ID', 'IMAGEM_URL'];
+    protected $table = 'PRODUTO_IMAGEM';
+    protected $primaryKey = 'produto_id';
+}
