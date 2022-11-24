5770ed4dd46dd64dd222e1101411655a4cb005cf
Oroch <oliveirarocha_pedro@hotmail.com>
2022-11-23 21:49:38 -0300

    FOOTER AJUSTADO

--- resources/views/cart/index.blade.php
+++ resources/views/cart/index.blade.php
@@ -7,75 +7,81 @@
         }
     </script>
 <link href="//fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<div class="container">
-    <div class="row">
-        <h3>Carrinho de compras</h3>
-        @if (count($cart) == 0)
-        <div>
-            <p>Seu carrinho de compras está vazio</p>
-            <img src="/img/cart.png" width="250" height="250">
+<main>
+    <div class="container">
+        <div class="row">
+            <h3>Carrinho de compras</h3>
+            @if (count($cart) == 0)
             <div>
-            <a class="backButton" href="../">Voltar</a>
+                <p>Seu carrinho de compras está vazio</p>
+                <img src="/img/cart.png" width="250" height="250">
+                <div>
+                <a class="backButton" href="../">Voltar</a>
+                </div>
             </div>
+            @endif
+            @foreach ($cart as $item)
+            <div class="divider"></div>
+            <div class="row col s12 m12 l12">
+                    <form method="POST" action="">
+
+                        <input type="hidden" name="pedido_id" value="">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th colspan="2"></th>
+                                    <th>Produto</th>
+                                    <th>Valor</th>
+                                    <th>Desconto</th>
+                                    <th>Total</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+
+                                <tr>
+                                    <td>
+                                        <img width="100" height="100" src="/img/banner2.jpg">
+                                    </td>
+                                    <td class="center-align">
+                                        <div class="center-align">
+                                            <a class="col l4 m4 s4" href="#" onclick="carrinhoRemoverProduto()">
+                                                <i class="material-icons small">remove_circle_outline</i>
+                                            </a>
+                                            <span class="col l4 m4 s4"> {{ $item->ITEM_QTD }}</span>
+                                            <a class="col l4 m4 s4" href="#" onclick="carrinhoAdicionarProduto()">
+                                                <i class="material-icons small">add_circle_outline</i>
+                                            </a>
+                                        </div>
+                                        <a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Retirar produto do carrinho?">Retirar produto</a>
+                                    </td>
+                                    <td>{{ $item->Produto->PRODUTO_NOME }}</td>
+                                    <td>{{ $item->Produto->PRODUTO_PRECO }}</td>
+                                    <td>{{ $item->Produto->PRODUTO_DESCONTO }}</td>
+                                    <td>Total</td>
+                                </tr>
+                            </tbody>
+                            <tfoot>
+
+
+                            </tfoot>
+                        </table>
+                    </form>
+            </div>
+            @endforeach
+            @if (count($cart) > 0)
+            <tr>
+                <td colspan="3"></td>
+                <td><strong>Total do pedido</strong></td>
+                <td></td>
+                <td>Total</td>
+            </tr>
+            @endif
         </div>
-        @endif
-        @foreach ($cart as $item)
-        <div class="divider"></div>
-        <div class="row col s12 m12 l12">
-                <form method="POST" action="">
 
-                    <input type="hidden" name="pedido_id" value="">
-                    <table>
-                        <thead>
-                            <tr>
-                                <th colspan="2"></th>
-                                <th>Produto</th>
-                                <th>Valor</th>
-                                <th>Desconto</th>
-                                <th>Total</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-
-                            <tr>
-                                <td>
-                                    <img width="100" height="100" src="/img/banner2.jpg">
-                                </td>
-                                <td class="center-align">
-                                    <div class="center-align">
-                                        <a class="col l4 m4 s4" href="#" onclick="carrinhoRemoverProduto()">
-                                            <i class="material-icons small">remove_circle_outline</i>
-                                        </a>
-                                        <span class="col l4 m4 s4"> {{ $item->ITEM_QTD }}</span>
-                                        <a class="col l4 m4 s4" href="#" onclick="carrinhoAdicionarProduto()">
-                                            <i class="material-icons small">add_circle_outline</i>
-                                        </a>
-                                    </div>
-                                    <a href="#" class="tooltipped" data-position="right" data-delay="50" data-tooltip="Retirar produto do carrinho?">Retirar produto</a>
-                                </td>
-                                <td>{{ $item->Produto->PRODUTO_NOME }}</td>
-                                <td>{{ $item->Produto->PRODUTO_PRECO }}</td>
-                                <td>{{ $item->Produto->PRODUTO_DESCONTO }}</td>
-                                <td>Total</td>
-                            </tr>
-                        </tbody>
-                        <tfoot>
-
-                        </tfoot>
-                    </table>
-                </form>
-        </div>
-        @endforeach
-        @if (count($cart) > 0)
-        <tr>
-            <td colspan="3"></td>
-            <td><strong>Total do pedido</strong></td>
-            <td></td>
-            <td>Total</td>
-        </tr>
-        @endif
     </div>
 
-</div>
+    @endsection
+</main>
 
-@endsection
+@yield('footer')
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -71,77 +71,79 @@
         </nav>
 
         @yield('main')
+
+        <footer id="tempaltemo_footer">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-md-4 pt-5">
+                        <a href="#"><img src="/img/dark.png" alt="B4F_logo" width="80"></a>
+                        <ul class="list-unstyled text-light footer-link-list">
+                            <li>
+                                <i class="fas fa-map-marker-alt fa-fw"></i>
+                                Rua Sol da Meia-Noite, 123
+                            </li>
+                            <li>
+                                <i class="fa fa-phone fa-fw"></i>
+                                <a class="text-decoration-none" href="tel:010-020-0340">+55 11 7070-7070</a>
+                            </li>
+                            <li>
+                                <i class="fa fa-envelope fa-fw"></i>
+                                <a class="text-decoration-none" href="mailto:info@company.com">faleconosco@bravo4fun.com</a>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4 pt-5">
+                        <h2 class="h2 text-light border-bottom pb-3 border-light">Categorias</h2>
+                        <ul class="list-unstyled text-light footer-link-list">
+                            <li><a class="text-decoration-none" href="#">Shows</a></li>
+                            <li><a class="text-decoration-none" href="#">Teatro</a></li>
+                            <li><a class="text-decoration-none" href="#">Infantil</a></li>
+                            <li><a class="text-decoration-none" href="#">Stand-up comedy</a></li>
+                            <li><a class="text-decoration-none" href="#">Eventos</a></li>
+                        </ul>
+                    </div>
+
+                    <div class="col-md-4 pt-5">
+                        <h2 class="h2 text-light border-bottom pb-3 border-light">Cidades</h2>
+                        <ul class="list-unstyled text-light footer-link-list">
+                            <li><a class="text-decoration-none" href="#">São Paulo</a></li>
+                            <li><a class="text-decoration-none" href="#">Belo Horizonte</a></li>
+                            <li><a class="text-decoration-none" href="#">Rio de Janeiro</a></li>
+                            <li><a class="text-decoration-none" href="#">Salvador</a></li>
+                            <li><a class="text-decoration-none" href="#">Campinas</a></li>
+                        </ul>
+                    </div>
+
+                </div>
+
+                <div class="row text-light mb-4">
+                    <div class="col-12 mb-3">
+                        <div class="w-100 my-3 border-top border-light"></div>
+                    </div>
+                    <div class="col-auto me-auto">
+                        <ul class="list-inline text-left footer-icons">
+                            <li class="list-inline-item border border-light rounded-circle text-center">
+                                <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
+                            </li>
+                            <li class="list-inline-item border border-light rounded-circle text-center">
+                                <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
+                            </li>
+                            <li class="list-inline-item border border-light rounded-circle text-center">
+                                <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
+                            </li>
+                            <li class="list-inline-item border border-light rounded-circle text-center">
+                                <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+
+
 </body>
-
-    <footer id="tempaltemo_footer">
-        <div class="container">
-            <div class="row">
-
-                <div class="col-md-4 pt-5">
-                    <a href="#"><img src="/img/dark.png" alt="B4F_logo" width="80"></a>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li>
-                            <i class="fas fa-map-marker-alt fa-fw"></i>
-                            Rua Sol da Meia-Noite, 123
-                        </li>
-                        <li>
-                            <i class="fa fa-phone fa-fw"></i>
-                            <a class="text-decoration-none" href="tel:010-020-0340">+55 11 7070-7070</a>
-                        </li>
-                        <li>
-                            <i class="fa fa-envelope fa-fw"></i>
-                            <a class="text-decoration-none" href="mailto:info@company.com">faleconosco@bravo4fun.com</a>
-                        </li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Categorias</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="#">Shows</a></li>
-                        <li><a class="text-decoration-none" href="#">Teatro</a></li>
-                        <li><a class="text-decoration-none" href="#">Infantil</a></li>
-                        <li><a class="text-decoration-none" href="#">Stand-up comedy</a></li>
-                        <li><a class="text-decoration-none" href="#">Eventos</a></li>
-                    </ul>
-                </div>
-
-                <div class="col-md-4 pt-5">
-                    <h2 class="h2 text-light border-bottom pb-3 border-light">Cidades</h2>
-                    <ul class="list-unstyled text-light footer-link-list">
-                        <li><a class="text-decoration-none" href="#">São Paulo</a></li>
-                        <li><a class="text-decoration-none" href="#">Belo Horizonte</a></li>
-                        <li><a class="text-decoration-none" href="#">Rio de Janeiro</a></li>
-                        <li><a class="text-decoration-none" href="#">Salvador</a></li>
-                        <li><a class="text-decoration-none" href="#">Campinas</a></li>
-                    </ul>
-                </div>
-
-            </div>
-
-            <div class="row text-light mb-4">
-                <div class="col-12 mb-3">
-                    <div class="w-100 my-3 border-top border-light"></div>
-                </div>
-                <div class="col-auto me-auto">
-                    <ul class="list-inline text-left footer-icons">
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="http://facebook.com/"><i class="fab fa-facebook-f fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://www.instagram.com/"><i class="fab fa-instagram fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://twitter.com/"><i class="fab fa-twitter fa-lg fa-fw"></i></a>
-                        </li>
-                        <li class="list-inline-item border border-light rounded-circle text-center">
-                            <a class="text-light text-decoration-none" target="_blank" href="https://www.linkedin.com/"><i class="fab fa-linkedin fa-lg fa-fw"></i></a>
-                        </li>
-                    </ul>
-                </div>
-            </div>
-        </div>
-    </footer>
 
     <script src="/js/bootstrap.bundle.min.js"></script>
     <script src="/js/templatemo.js"></script>
